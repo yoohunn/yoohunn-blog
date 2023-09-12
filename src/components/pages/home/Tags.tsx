76ab4a3319acc842b66797687cd42b4b63300e97
YooHyunJ <poirtmove@gmail.com>
2023-09-12 22:07:03 +0900

    feat: Tags 아이템 추가

--- src/components/pages/home/Tags.tsx
+++ src/components/pages/home/Tags.tsx
@@ -1,7 +1,7 @@
 export function Tags() {
   return (
     <ul className='mt-10 flex flex-wrap gap-2.5 w-full'>
-      {['All', 'Tags', 'Collections'].map((item) => (
+      {['All', 'Posts', 'Tags', 'Collections'].map((item) => (
         <li
           key={item}
           className='h-9 px-3 flex-center rounded-lg bg-gray-100/60 border border-gray-200/50 text-sm md:text-base'
